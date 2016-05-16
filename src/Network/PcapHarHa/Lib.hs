6892b26a632aad20cb6a2b6f85ebf3824651c401
Richard Towers <richard@richard-towers.com>
2016-05-16 18:44:36 +0100

    Print first packet for debugging

--- src/Network/PcapHarHa/Lib.hs
+++ src/Network/PcapHarHa/Lib.hs
@@ -1,13 +1,34 @@
 module Network.PcapHarHa.Lib ( readPcap ) where
 
+import Data.IORef
 import Network.Pcap
+import qualified Data.ByteString.Char8 as C
+
+printPcap :: [(PktHdr, C.ByteString)] -> IO ()
+printPcap xs = do
+    let (hdr, bdy) = head xs
+    print $ hdr
+    C.putStrLn $ bdy
 
 readPcap :: IO ()
 readPcap = do
     handle <- openOffline "example.pcap"
-    _ <- dispatch handle (-1) pcapCallback
-    return ()
+    setFilter handle pcapFilter True 0xff
+    packetStore <- newIORef []
+    _ <- dispatch handle (-1) (pcapCallback $ storePacket packetStore)
+    packets <- readIORef packetStore
+    printPcap $ reverse packets
 
-pcapCallback :: Callback
-pcapCallback header word = putStrLn "packet"
+storePacket :: IORef [(PktHdr, C.ByteString)] -> (PktHdr, C.ByteString) -> IO ()
+storePacket ref x = modifyIORef ref (x:)
+
+-- Based on the examples in `man pcap-filter`.
+-- Selects only IPv4 HTTP requests / responses
+pcapFilter :: String
+pcapFilter = "tcp and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)"
+
+pcapCallback :: ((PktHdr, C.ByteString) -> IO ()) -> Callback
+pcapCallback store header word = do
+    packet <- toBS (header, word)
+    store packet
 
