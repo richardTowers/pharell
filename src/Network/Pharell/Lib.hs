657e4da41759ea250b759af93cf7f92fb2ca31a5
Richard Towers <richard@richard-towers.com>
2016-06-01 22:06:59 +0100

    Group packets by source and destination ports for analysis

--- src/Network/Pharell/Lib.hs
+++ src/Network/Pharell/Lib.hs
@@ -1,18 +1,29 @@
 module Network.Pharell.Lib ( readPcap ) where
 
+import           Control.Arrow           ((&&&))
 import qualified Data.ByteString.Char8   as C
+import           Data.Function           (on)
 import           Data.IORef
-import           Network.Pcap
-import           Network.Pharell.Packets ( parseByteString, Packet(..) )
+import           Data.List               (groupBy, sortBy)
+import           Data.Ord                (comparing)
 import           Data.TCP
-import           Control.Arrow
+import           Network.Pcap
+import           Network.Pharell.Packets (Packet (..), parseByteString)
 
 printPackets :: [(PktHdr, C.ByteString)] -> IO ()
 printPackets xs = do
     let packets = map (parseByteString . snd) xs
-    let tcpHeaders = map tcpHeader packets
-    let seqs = map (seqNumber &&& ackNumber) tcpHeaders
-    mapM_ print seqs
+
+    -- TODO: this "algorithm" depends on a sorted list to group tcp streams.
+    -- This means that streaming input isn't going to work. Fine while we're
+    -- exploring though.
+    let sortedPackets = sortBy (comparing (sp &&& dp)) packets
+    let groupedPackets = groupBy ((==) `on` (sp &&& dp)) sortedPackets
+
+    mapM_ (print . (sp &&& dp) . head) groupedPackets
+
+    where sp = srcPort . tcpHeader
+          dp = dstPort . tcpHeader
 
 readPcap :: IO ()
 readPcap = do
